61347905cda56028bea872fff2398487bf5f3d43
gharam37 <g_z48@yahoo.com>
2019-04-20 21:04:45 +0200

    frontend for emotions

--- platform-frontend/src/Recording.js
+++ platform-frontend/src/Recording.js
@@ -1,7 +1,12 @@
 import React, {Component} from 'react';
 import video from './vid.mp4'
+import happy from './happy.jpg'
+import sad from './sad.jpg'
+
+
 import io from 'socket.io-client'
 
+import StarRatingComponent from 'react-star-rating-component';
 
 /* Import Components */
 
@@ -11,32 +16,56 @@ class FormContainer extends Component {
     super(props);
 
     this.state = {
-      playing:false
+      playing:false,
+      valence:1,
+      arousal:1,
+      emotion:0
+
 
 
 
     }
     this.handleFormSubmit = this.handleFormSubmit.bind(this);
+    this.onHappyClick = this.onHappyClick.bind(this);
+    this.onSadClick = this.onSadClick.bind(this);
 
         this.playVideo=this.playVideo.bind(this)
 
 
   }
+  onValenceClick(nextValue, prevValue, name) {
+      this.setState({valence: nextValue});
+      console.log(nextValue)
 
+  }
+onArousalClick(nextValue, prevValue, name) {
+        this.setState({arousal: nextValue});
+
+}
+onEmotionClick(nextValue, prevValue, name) {
+          this.setState({emotion: nextValue});
+
+}
+onHappyClick() {
+    this.setState({emotion: 0});
+    console.log(this.state.emotion)
+
+}
+onSadClick(nextValue, prevValue, name) {
+  this.setState({emotion:1});
+  console.log(this.state.emotion)
+}
   /* This life cycle hook gets executed when the component mounts */
 
   handleFormSubmit(e) {
 
   e.preventDefault();
-
   fetch('http://localhost:3030/api/Valences',{
       method: "POST",
       body: JSON.stringify({
-        name:'10',
-        userID:'',
-        value:0
-
-
+        name:this.props.location.state.newUser.name,
+        userID:this.props.location.state.newUser.id,
+        value:this.state.valence
       }),
       headers: {
         'Accept': 'application/json',
@@ -44,32 +73,50 @@ class FormContainer extends Component {
       },
     }).then(response => {
       response.json().then(data =>{
-        console.log("Successful" + data);
+           console.log("Successful " + data);
       })
   })
+
+  fetch('http://localhost:3030/api/Arousals',{
+      method: "POST",
+      body: JSON.stringify({
+        name:this.props.location.state.newUser.name,
+        userID:this.props.location.state.newUser.id,
+        value:this.state.arousal
+      }),
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+    }).then(response => {
+      response.json().then(data =>{
+           console.log("Successful " + data);
+      })
+  })
+
+
+  fetch('http://localhost:3030/api/Emotions',{
+      method: "POST",
+      body: JSON.stringify({
+        name:this.props.location.state.newUser.name,
+        userID:this.props.location.state.newUser.id,
+        value:this.state.emotion
+      }),
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+    }).then(response => {
+      response.json().then(data =>{
+           console.log("Successful " + data);
+      })
+  })
+
   }
 
   playVideo() {
     this.video.play()
 
-    fetch('http://localhost:3030/api/Emotions',{
-        method: "POST",
-        body: JSON.stringify({
-          name:'10',
-          userID:'',
-          value:0
-
-
-        }),
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        },
-      }).then(response => {
-        response.json().then(data =>{
-          console.log("From video" + data);
-        })
-    })
 
 
 }
@@ -99,8 +146,39 @@ componentWillUnmount() {
       <p>
         Welcome to the expirement
       </p>
+      <div>
+
       <video ref={ref}  src={video}  type="video/mp4"></video>
     <button id='playButton'  onClick={this.playVideo}>Whenever you are ready!</button>
+    </div>
+
+
+    <div>
+        <h2>How was your valence: {this.state.valence}</h2>
+        <StarRatingComponent
+          name="rate1"
+          starCount={5}
+          value={this.state.valence}
+          onStarClick={this.onValenceClick.bind(this)}
+        />
+        <h2>How was your arousal: {this.state.arousal}</h2>
+        <StarRatingComponent
+          name="rate1"
+          starCount={5}
+          value={this.state.arousal}
+          onStarClick={this.onArousalClick.bind(this)}
+        />
+
+        <div>
+
+        <button onClick={this.onHappyClick}><img src={happy} alt="my image"  /></button>
+        <button onClick={this.onSadClick}><img src={sad} alt="my image"  /></button>
+        </div>
+
+
+        <button id='playButton'  onClick={this.handleFormSubmit}>Submit your results!</button>
+
+      </div>
 
       </div>
     );
